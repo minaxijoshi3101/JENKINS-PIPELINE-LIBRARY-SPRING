79332527bb2fa0745cd426ef81a1459bd615e249
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2021-08-01 23:50:22 +0530

    build_and_deployment_pipeline.groovy created

---
+++ vars/build_and_deployment_pipeline.groovy
@@ -0,0 +1,29 @@
+def call(Map pipelineParams)
+{
+  try
+  {
+    timeout(time: 60,unit:"MINUTES")
+    {
+      env.BRNACH = pipelineParams.BRANCH
+      env.REPO = pipelineParams.REPO
+      pipeline
+      {
+        new environmentVars().call(pipelineParams)
+        node(pipelineParams.BUILD_NODE)
+        {
+          stage("checkout source code")
+          {
+            new checkoutSCM().call(pipelineParams)
+          }
+          stage("Build provisioning")
+          {
+            new buildCompileApp().call(pipelineParams)
+          }
+        }
+      }
+    }
+  }
+  catch()
+  {
+  }
+}
