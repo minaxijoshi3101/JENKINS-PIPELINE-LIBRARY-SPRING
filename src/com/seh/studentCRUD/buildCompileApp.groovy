a631a61b2207bc4bf7f4f02cd068a983ab58200e
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2021-08-08 16:18:16 +0530

    Create buildCompileApp.groovy

---
+++ src/com/seh/studentCRUD/buildCompileApp.groovy
@@ -0,0 +1,13 @@
+def call(Map pipelineParams) 
+{
+  if(pipelineParams.APP_TYPE == "java")
+  POM = readMavenPom file: "${REPO}/pom.xml"
+  env.POM_VERSION = POM.version
+{
+  sh '''
+  cd $REPO
+  mvn clean install
+  cp 
+  '''
+}
+}
