74d07ac31e3970a6d59f88d5c393ba7b26212741
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2021-08-10 20:16:43 +0530

    Update buildCompileApp.groovy

--- src/com/seh/studentCRUD/buildCompileApp.groovy
+++ src/com/seh/studentCRUD/buildCompileApp.groovy
@@ -3,9 +3,9 @@ package com.seh.studentCRUD
 def call(Map pipelineParams) 
 {
   if(pipelineParams.APP_TYPE == "java")
-  POM = readMavenPom file: "${REPO}/pom.xml"
+  {
+  POM = readMavenPom file: REPO+"/pom.xml"
   env.POM_VERSION = POM.version
-{
   sh '''
   cd $REPO
   mvn clean install
