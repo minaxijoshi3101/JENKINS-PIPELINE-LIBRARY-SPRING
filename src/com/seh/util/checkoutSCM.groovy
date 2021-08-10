582f75ccefd424a4318370f07ad95ff96cd539d1
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2021-08-10 15:46:29 +0530

    Update checkoutSCM.groovy

--- src/com/seh/util/checkoutSCM.groovy
+++ src/com/seh/util/checkoutSCM.groovy
@@ -4,6 +4,8 @@ def call(Map pipelineParams)
 {
   env.SCM_URL = "git@github.com:"+pipelineParams.GIT_GROUP+"/"+pipelineParams.REPO+".git"
   echo "code checkout from SCM"
+  echo pipelineParams.GIT_GROUP
+  echo pipelineParams.REPO
   sh '''
   rm -rf ${REPO}
   git clone --single-branch --branch ${BRANCH} ${SCM_URL}
