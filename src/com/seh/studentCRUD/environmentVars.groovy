958712b952c0e20994ed6d42c483025bb6d4ae7e
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2021-08-10 15:31:10 +0530

    Update environmentVars.groovy

--- src/com/seh/studentCRUD/environmentVars.groovy
+++ src/com/seh/studentCRUD/environmentVars.groovy
@@ -1,1 +1,15 @@
 package com.seh.studentCRUD
+
+def call(Map pipelineParams) {
+    env.REPO = pipelineParams.REPO
+    env.DOCKER_HOST = pipelineParams.DOCKER_HOST
+    env.DOCKER_REGISTRY = pipelineParams.DOCKER_REGISTRY
+    env.APP_NAME = pipelineParams.APP_NAME
+    env.CONTAINER_PORT = pipelineParams.CONTAINER_PORT
+    env.NAMESPACE = pipelineParams.NAMESPACE
+    env.HOST_PORT = pipelineParams.HOST_PORT
+    env.HOST_IP  = pipelineParams.HOST_IP   
+    env.REPLICAS = pipelineParams.REPLICAS 
+    env.ENVIRONMENT = pipelineParams.ENVIRONMENT
+    env.DB_NAME = pipelineParams.DB_NAME
+}
