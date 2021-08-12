5728f991da5120fc3203e4e013503ddd40ce0d74
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2021-08-12 09:43:15 +0530

    Update deployApp.groovy

--- src/com/seh/studentCRUD/deployApp.groovy
+++ src/com/seh/studentCRUD/deployApp.groovy
@@ -3,6 +3,6 @@ package com.seh.studentCRUD
 {
   sh '''
   sudo docker rm -f ${APP_NAME}
-  sudo docker run -d --name ${APP_NAME} -p ${HOST_PORT}:${CONTAINER_PORT} -v myvol1:/var/JENKINS_HOME ${DOCKER_REGISTRY}:latest
+  sudo docker run -v testvol1:/etc -d --name ${APP_NAME} -p ${HOST_PORT}:${CONTAINER_PORT} -v myvol1:/var/JENKINS_HOME ${DOCKER_REGISTRY}:latest
   '''
 }
